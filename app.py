a4fbdf2fb4e9fd72a33ac8d67939de56bd1b76f4
Álvaro Vaca Ferreras <avacaferreras@gmail.com>
2020-05-17 20:22:30 +0200

    Añadido historial.html.

--- app.py
+++ app.py
@@ -22,6 +22,19 @@ def status():
 def invocador():
     return render_template("invocador.html")
 
+@app.route('/invocador/<summoner>',methods=["GET"])
+def invocador2(summoner):
+    payload = {'api_key':app.config["key"]}
+    r=requests.get(app.config["URL_BASE"]+"summoner/v4/summoners/by-name/"+summoner+"", params=payload)
+    champs=requests.get("http://ddragon.leagueoflegends.com/cdn/10.9.1/data/en_US/champion.json")
+    if r.status_code == 200:
+        accountid=r.json()["accountId"]
+        history=requests.get(app.config["URL_BASE"]+"match/v4/matchlists/by-account/"+accountid+"", params=payload)
+        if history.status_code == 200:
+            return render_template("historial.html", peticion=history.json(), nombre=summoner, champs=champs.json())
+    else:
+        return render_template("historial.html", nombre=summoner, fallo=True)
+
 @app.route('/top',methods=["GET","POST"])
 def top():
     tiers=["CHALLENGER","GRANDMASTER","MASTER","DIAMOND","PLATINUM","GOLD","SILVER","BRONZE","IRON"]
@@ -40,7 +53,6 @@ def top():
         else:
             return render_template("top.html", tiers=tiers, tierstr=tierstr, divisions=divisions, fallo=True, tier=tierstr[tiers.index(tier)], division=division, page=page)
 
-#url=r.url.replace("page="+str(payload["page"])+"","page="+str(payload["page"]+1)+"")
 #port=os.environ["PORT"]
 #app.run('0.0.0.0', int(port), debug=False)
 app.run(debug=True)
