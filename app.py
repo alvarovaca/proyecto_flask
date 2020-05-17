93a7639ad18707131241e1b6f496e3d2cb6a5fb8
Álvaro Vaca Ferreras <avacaferreras@gmail.com>
2020-05-17 12:20:30 +0200

    Progreso en la página de estado.

--- app.py
+++ app.py
@@ -1,39 +1,18 @@
-from flask import Flask, render_template, request, abort
-import json
+from flask import Flask, render_template, request
+import requests
 import os
 app = Flask(__name__)
 
-with open("MSX.json") as fichero:
-    datos=json.load(fichero)
-
-@app.route('/',methods=["GET"])
+@app.route('/status',methods=["GET"])
 def inicio():
-	return render_template("inicio.html")
+    URL_BASE="https://euw1.api.riotgames.com/lol/"
+    key=os.environ["key"]
+    payload = {'api_key':key}
+    r=requests.get(URL_BASE+"status/v3/shard-data", params=payload)
+    if r.status_code == 200:
+        return render_template("status.html", peticion=r.json())
+    return render_template("status.html")
 
-@app.route('/juegos',methods=["GET","POST"])
-def juegos():
-    categorias=[]
-    categorias.append("")
-    for i in datos:
-        if i["categoria"] not in categorias:
-            categorias.append(i["categoria"])
-    categorias.sort()
-    if request.method=="GET":
-        return render_template("juegos.html",categorias=categorias)
-    else:
-        nombre=request.form.get("name")
-        categoria=request.form.get("category")
-        for i in datos:
-            if (nombre == "" or str(i["nombre"]).startswith(nombre)) and (categoria == "" or categoria == i["categoria"]):
-                return render_template('juegos.html',juegos=datos,nombre=nombre,categoria=categoria,categorias=categorias)
-        return render_template('juegos.html',nombre=nombre,categoria=categoria,categorias=categorias)
-
-@app.route('/juego/<int:identificador>',methods=["GET"])
-def juego(identificador):
-    for i in datos:
-        if i["id"] == identificador:
-            return render_template('juego.html',juego=i)
-    abort(404)
-
-port=os.environ["PORT"]
-app.run('0.0.0.0', int(port), debug=False)
+#port=os.environ["PORT"]
+#app.run('0.0.0.0', int(port), debug=False)
+app.run(debug=True)
